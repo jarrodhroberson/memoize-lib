4d8f46a622eb076991de36310094ebeb78ab1ac0
kzvpfh <jarrod.roberson@gm.com>
2019-11-13 14:22:52 -0500

    adding WeakReference to the memoized value

--- src/main/java/com/vertigrated/memoize/Memoizer.java
+++ src/main/java/com/vertigrated/memoize/Memoizer.java
@@ -1,24 +1,44 @@
 package com.vertigrated.memoize;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreType;
 import com.google.common.base.Objects;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.annotation.concurrent.Immutable;
+import java.lang.ref.WeakReference;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * This class Memoizes (caches) the results of a Function that takes a type T
+ * and returns a R without having to recalculating R over and over.
+ *
+ * Specifically it can take an object and create a String representation and
+ * cache that representation without having to re-convert it over and over.
+ *
+ * @param <T> Type of source to be memoized
+ * @param <R> Type of the value that is memoized
+ */
+@JsonIgnoreType
 @Immutable
 public class Memoizer<T, R> implements Supplier<R>
 {
+    @Nonnull
     private final ReentrantLock lock = new ReentrantLock();
+    @Nonnull
     private final Predicate<R> predicate;
+    @Nonnull
     private final Supplier<T> supplier;
+    @Nonnull
     private final Function<T, R> function;
-    private final AtomicReference<R> value;
+    @Nonnull
+    private final AtomicReference<WeakReference<R>> value;
 
     public Memoizer(@Nonnull final Function<T,R> function, @Nonnull final T source)
     {
@@ -26,7 +46,7 @@ public class Memoizer<T, R> implements Supplier<R>
             @Override
             public T get()
             {
-                return source;
+                return checkNotNull(source);
             }
         });
     }
@@ -42,12 +62,12 @@ public class Memoizer<T, R> implements Supplier<R>
         }, function, Supplier);
     }
 
-    Memoizer(@Nonnull final Predicate<R> predicate, @Nonnull final Function<T,R> function, @Nonnull final Supplier<T> Supplier)
+    private Memoizer(@Nonnull final Predicate<R> predicate, @Nonnull final Function<T,R> function, @Nonnull final Supplier<T> Supplier)
     {
         this.predicate = predicate;
         this.supplier = Supplier;
         this.function = function;
-        this.value = new AtomicReference<>();
+        this.value = new AtomicReference<>(new WeakReference<R>(null));
     }
 
     @Nonnull
@@ -57,11 +77,11 @@ public class Memoizer<T, R> implements Supplier<R>
         this.lock.lock();
         try
         {
-            if (this.predicate.test(this.value.get()))
+            if (this.predicate.test(this.value.get().get()))
             {
-                this.value.set(this.function.apply(this.supplier.get()));
+                this.value.set(new WeakReference<>(this.function.apply(this.supplier.get())));
             }
-            return this.value.get();
+            return checkNotNull(this.value.get().get());
         }
         finally
         {
@@ -70,24 +90,24 @@ public class Memoizer<T, R> implements Supplier<R>
     }
 
     @Override
-    public boolean equals(final Object o)
+    public boolean equals(@Nullable final Object o)
     {
         if (this == o) { return true; }
         if (o == null || getClass() != o.getClass()) { return false; }
-        final Memoizer<?, ?> memoizer = (Memoizer<?, ?>) o;
-        return Objects.equal(this.value.get(), memoizer.value.get());
+        return Objects.equal(this.value.get().get(), ((Memoizer<?, ?>) o).value.get().get());
     }
 
     @Override
     public int hashCode()
     {
-        return Objects.hashCode(this.value.get());
+        return Objects.hashCode(this.value.get().get());
     }
 
+    @Nonnull
     @Override
     public String toString()
     {
-        return this.value.get().toString();
+        return this.value.get().get().toString();
     }
 
     public static final class ToString<T> extends Memoizer<T,String>
